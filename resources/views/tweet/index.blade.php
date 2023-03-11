fa525e230d3406c89be60c0815c976e4c968bbaa
sueaki3104 <sueaki@a-omega.co.jp>
2023-03-11 12:03:54 +0900

    2023-03-11AM

---
+++ resources/views/tweet/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>プレミアム価格撲滅委員会</title>
+</head>
+<body>
+    <h1>定価販売を応援しようって呟くアプリ</h1>
+    <p>{{ $name }}</p>
+</body>
+</html>
